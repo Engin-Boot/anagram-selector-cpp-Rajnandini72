91c403b1028d4387d523785277d5a3c683126312
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-09-07 18:55:00 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -13,14 +13,18 @@ bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& wor
     word2Sorted.erase(std::remove_if(word2Sorted.begin(), word2Sorted.end(), ::isspace), word2Sorted.end());
     int word1Length = word1Sorted.size();
     int word2Length = word2Sorted.size();
+    transform(word1Sorted.begin(), word1Sorted.end(), word1Sorted.begin(), ::tolower);
+    transform(word2Sorted.begin(), word2Sorted.end(), word2Sorted.begin(), ::tolower);
+    if (word1Sorted.compare(word2Sorted) == 0)
+        return false;
     if (word1Length ==1 || word2Length ==1)
         return false;
     else if (word1Length == word2Length)
     {
-        transform(word1Sorted.begin(), word1Sorted.end(), word1Sorted.begin(), ::tolower);
+        /*transform(word1Sorted.begin(), word1Sorted.end(), word1Sorted.begin(), ::tolower);
         transform(word2Sorted.begin(), word2Sorted.end(), word2Sorted.begin(), ::tolower);
         if (word1Sorted.compare(word2Sorted) == 0)
-            return false;
+            return false;*/
         std::sort(word1Sorted.begin(), word1Sorted.end());
         std::sort(word2Sorted.begin(), word2Sorted.end());
        
