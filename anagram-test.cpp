f471586954fb8205c9cd12f528893b3bd8004ac7
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-09-07 18:41:29 +0530

    Update anagram-test.cpp

--- anagram-test.cpp
+++ anagram-test.cpp
@@ -3,12 +3,40 @@
 #include "catch.hpp"
 #include "anagram.h"
 
-TEST_CASE("recognizes shuffled alphabets as anagrams") {
+
+TEST_CASE("recognizes shuffled alphabets as anagrams")
+{
     REQUIRE(Anagram::WordPairIsAnagram("restful", "fluster") == true);
     REQUIRE(Anagram::WordPairIsAnagram("forty five", "over fifty") == true);
 }
 
-TEST_CASE("reports non-anagrams") {
+TEST_CASE("reports identical words as non-anagrams")
+{
+    REQUIRE(Anagram::WordPairIsAnagram("anagrams", "anAGrams") == false);
+    REQUIRE(Anagram::WordPairIsAnagram("forty five", "forty five") == false);
+}
+
+
+TEST_CASE("reports completely different word of same/different length with base word as non-anagrams")
+{
+    REQUIRE(Anagram::WordPairIsAnagram("anAGram", "nanDini") == false);
+    REQUIRE(Anagram::WordPairIsAnagram("anagram", "rAj nandini") == false);
+}
+
+TEST_CASE("reports for shortest non-anagrams containing only one character")
+{
+    REQUIRE(Anagram::WordPairIsAnagram("a", "A") == false);
+}
+
+
+TEST_CASE("reports for shortest anagrams containing only two characters")
+{
+    REQUIRE(Anagram::WordPairIsAnagram("No", "On") == true);
+}
+
+
+TEST_CASE("reports non-anagrams") 
+{
     REQUIRE(Anagram::WordPairIsAnagram("something", "else") == false);
 }
 
@@ -17,8 +45,14 @@ TEST_CASE("recognizes anagrams when there are case- and space- differences") {
         == true);
 }
 
-TEST_CASE("selects anagrams of a word") {
+TEST_CASE("selects anagrams of a word from given vector") {
     auto selection = Anagram::SelectAnagrams("master",
-        {"stream", "something", "maters"});
+        { "stream", "something", "maters" });
     REQUIRE(selection == std::vector<std::string>{"stream", "maters"});
+}
+
+TEST_CASE("reports if no angram word found from given vector") {
+    auto selection = Anagram::SelectAnagrams("master",
+        { "vampire DiaRies","Dragontales", "stuard Little", "abc" ,"D"});
+    REQUIRE(selection == std::vector<std::string>{});
 }
