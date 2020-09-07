e5d3ec22744e66aff83160c181f4f10ae2fee6f3
Rajnandini Dandane <54036309+Rajnandini72@users.noreply.github.com>
2020-09-07 18:40:32 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -13,16 +13,20 @@ bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& wor
     word2Sorted.erase(std::remove_if(word2Sorted.begin(), word2Sorted.end(), ::isspace), word2Sorted.end());
     int word1Length = word1Sorted.size();
     int word2Length = word2Sorted.size();
-    if (word1Length == word2Length)
+    if (word1Length ==1 || word2Length ==1)
+        return false;
+    else if (word1Length == word2Length)
     {
         transform(word1Sorted.begin(), word1Sorted.end(), word1Sorted.begin(), ::tolower);
         transform(word2Sorted.begin(), word2Sorted.end(), word2Sorted.begin(), ::tolower);
+        if (word1Sorted.compare(word2Sorted) == 0)
+            return false;
         std::sort(word1Sorted.begin(), word1Sorted.end());
         std::sort(word2Sorted.begin(), word2Sorted.end());
        
         if (word1Sorted.compare(word2Sorted) == 0)
             return true;
-        return false;   
+        return false;
         
     }
     return false;
@@ -36,22 +40,9 @@ std::vector<std::string> Anagram::SelectAnagrams(
     std::vector<std::string> areAnagrams = {};
     for (unsigned int i = 0; i < candidates.size(); i++)
     {
-        std::string wordsort = word;
-        std::string s = candidates[i];
-        wordsort.erase(std::remove_if(wordsort.begin(), wordsort.end(), ::isspace), wordsort.end());
-        s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
-        int word1Length = wordsort.length();
-        int word2Length = s.length();
-        if (word1Length == word2Length)
-        {
-            transform(wordsort.begin(), wordsort.end(), wordsort.begin(), ::tolower);
-            transform(s.begin(), s.end(), s.begin(), ::tolower);
-            std::sort(wordsort.begin(), wordsort.end());
-            std::sort(s.begin(), s.end());
-
-            if (wordsort.compare(s) == 0)
-                areAnagrams.push_back(candidates[i]);
-        }
+        if(WordPairIsAnagram(word,candidates[i]))
+            areAnagrams.push_back(candidates[i]);
+        
     }
     return areAnagrams;
 }
